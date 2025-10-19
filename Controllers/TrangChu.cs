be9257c756e95aec8baa79448f60f96e59f316b3
DuongTrong35 <duongtrongtan352004@gmail.com>
2025-10-19 19:44:24 +0700

    Cập nhật trang chủ
    19h44 ngày 19/10/2025

---
+++ Controllers/TrangChu.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetailStore.Controllers
+{
+    public class TrangChu : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
