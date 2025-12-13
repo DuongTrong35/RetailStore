d3ac635699f372f5e303230965625e24e8a003fe
htuan2800 <huynhngoctuan48@gmail.com>
2025-12-13 20:39:53 +0700

    Images/Products

--- Controllers/SalesController.cs
+++ Controllers/SalesController.cs
@@ -27,7 +27,7 @@ namespace RetailStore.Controllers
                                ProductName = p.ProductName,
                                Price = p.Price,
                                CategoryId = (int)p.CategoryId,
-                               image = p.ImageUrl,
+                               image = p.ImageUrl ?? "/images/products/null-image.png",
                                Quantity = inv.Quantity ?? 0,
                                unit = p.Unit
                            };
